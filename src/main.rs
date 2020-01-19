7487410182ad4c9d36b4cc32305891e6aae27f5e
Talha-Tanveer <talhatanveer10@gmail.com>
2020-01-19 20:32:13 +0500

    commit 1 Q1

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+pub mod fruit_and_vegetable_shop {
+    pub mod fruit_corner{
+        pub fn types_of_oranges() {
+
+        }
+    }
+}
+
+use fruit_and_vegetable_shop::fruit_corner;
+fn main () 
+{
+    fruit_corner::types_of_oranges();
+}
